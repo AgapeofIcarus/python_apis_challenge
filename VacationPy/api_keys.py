dc08f3a5eb9740d0140f48ebad87e0e6262d9c86
Chase Webb <webb.chase.a@gmail.com>
2021-02-02 05:16:26 -0500

    fixed api issue

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4205aa957cf037cd14019e04075dc554
+"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAkr1NnbZq5hnNTVuMv96Fsylmr1LfHBGs"
